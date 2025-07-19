a09695907157b4b07560688f3ae5e83ef8debada
anjSingh453 <166209384+anjSingh453@users.noreply.github.com>
2025-07-19 12:04:21 +0530

    Update chatWindow.jsx

--- frontend/src/chatWindow.jsx
+++ frontend/src/chatWindow.jsx
@@ -29,7 +29,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("https://chatbot-web-x2q0.onrender.com/api/chat", options);
+            const response = await fetch("https://chatbot-web-x2q0.onrender.com/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
